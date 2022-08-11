3ea13bac5332444dcca481fcf762edbf82feb1dc
farrukh_kh <farrukhbekkhusainov@gmail.com>
2022-08-11 15:23:48 +0500

    Initial commit

---
+++ src/main/java/kh/farrukh/billypay/BillyPayApplication.java
@@ -0,0 +1,13 @@
+package kh.farrukh.billypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillyPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillyPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kh/farrukh/billypay/BillyPayApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.farrukh.billypay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillyPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
